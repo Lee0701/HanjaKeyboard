15cb3adea1141207adafef00ec3391dc76ac76e4
Lee0701 <hgl1002@naver.com>
2025-02-14 11:39:37 +0900

    Move language key to left

--- app/src/main/java/ee/oyatl/hanjakbd/keyboard/DefaultBottomRowKeyboard.kt
+++ app/src/main/java/ee/oyatl/hanjakbd/keyboard/DefaultBottomRowKeyboard.kt
@@ -9,17 +9,17 @@ class DefaultBottomRowKeyboard(
 ): DefaultKeyboard(listener) {
     override fun buildRows(context: Context): List<KbdRowBinding> {
         val bottomRow = buildRow(context, "")
-        bottomRow.root.addView(buildSpacer(context, 3.0f))
+        bottomRow.root.addView(buildSpacer(context, 2.0f))
+        bottomRow.root.addView(buildSpecialKey(
+            context,
+            R.drawable.baseline_language_24,
+            1.0f) { listener.onLanguage() })
         bottomRow.root.addView(buildSpecialKey(
             context,
             R.drawable.baseline_space_bar_24,
             4.0f
         ) { listener.onSpace() })
-        bottomRow.root.addView(buildSpecialKey(
-            context,
-            R.drawable.baseline_language_24,
-            1.0f) { listener.onLanguage() })
-        bottomRow.root.addView(buildSpacer(context, 2.0f))
+        bottomRow.root.addView(buildSpacer(context, 3.0f))
         return listOf(bottomRow)
     }
 }

7bf0145da188e2ee4f94ba8221e412f28051a585
Lee0701 <hgl1002@naver.com>
2025-08-19 11:59:04 +0900

    Add non-hangul hanja number conversion

--- app/src/main/java/ee/oyatl/hanjakbd/NonHangulConverter.kt
+++ app/src/main/java/ee/oyatl/hanjakbd/NonHangulConverter.kt
@@ -12,10 +12,37 @@ object NonHangulConverter {
         "<" to listOf("〈", "〉", "《", "》")
     )
 
+    val cjkNumberTable: Map<Char, Char> = mapOf(
+        '0' to '〇',
+        '1' to '一',
+        '2' to '二',
+        '3' to '三',
+        '4' to '四',
+        '5' to '五',
+        '6' to '六',
+        '7' to '七',
+        '8' to '八',
+        '9' to '九'
+    )
+
+    val cjkFullNumberTable: Map<Char, Char> = mapOf(
+        '0' to '零',
+        '1' to '壹',
+        '2' to '貳',
+        '3' to '參',
+        '4' to '髟',
+        '5' to '伍',
+        '6' to '陸',
+        '7' to '柒',
+        '8' to '捌',
+        '9' to '玖'
+    )
+
     fun convert(text: String): List<Candidate> {
         val result = mutableListOf<Candidate>()
         result += Candidate(-1, text, 0f)
         result += convertFullwidth(text)
+        result += convertCJKNumber(text)
         result += convertCJKPunct(text)
         return result.distinct()
     }
@@ -35,6 +62,15 @@ object NonHangulConverter {
         val result = mutableListOf<Candidate>()
         if(text in punctTable) result += punctTable.getValue(text).map { Candidate(-1, it, 0f, text.length) }
         if(text.firstOrNull().toString() in punctTable) result += punctTable.getValue(text.firstOrNull().toString()).map { Candidate(-1, it, 0f) }
+        return result
+    }
+
+    fun convertCJKNumber(text: String): List<Candidate> {
+        val result = mutableListOf<Candidate>()
+        val cjkNumber = text.mapNotNull { c -> cjkNumberTable[c] }.joinToString("")
+        if(cjkNumber.length == text.length) result += Candidate(-1, cjkNumber, 0f, cjkNumber.length)
+        val cjkFullNumber = text.mapNotNull { c -> cjkFullNumberTable[c] }.joinToString("")
+        if(cjkFullNumber.length == text.length) result += Candidate(-1, cjkFullNumber, 0f, cjkFullNumber.length)
         return result
     }
 }
